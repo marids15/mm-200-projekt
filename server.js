9895e2636bba2f1388831bffa8a374c148ecb579
Reggyboy <42719033+Reggyboy@users.noreply.github.com>
2018-11-01 17:11:54 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const user = require('./server/user.js');
+const db = require('./server/db.js');
 const app = express();
 
 app.set('port', (process.env.PORT || 8080));
 app.use(express.static('public'));
 app.use(bodyParser.json());
-app.listen(app.get('port'), function() {console.log('server is running', app.get('port'));});
+app.use(user);
+
+app.use(function(err, req, res, next) {
+  console.err(err.stack);
+  res.status(500).send('Error...');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('server is running', app.get('port'));
+});
