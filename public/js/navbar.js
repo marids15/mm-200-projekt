a7bd9537ac48010d5aed26ba96d08e96f8c26642
R-MAN\Ragnar <ragnab07@uia.no>
2018-11-06 14:01:36 +0100

    added new navbar
    ThistimeMaby

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -10,3 +10,18 @@ function expandNavigation(evt) {
     navbar.className = "navbar";
   }
 }
+// defining variables for making active part of navbar
+var a = "";
+var b = "";
+var c = "";
+// Makes navbar- and handles what is the active element
+function MakeNavbar(evt){
+  if (evt == 1){a = 'class="active"';}
+  if (evt == 2){b = 'class="active"';}
+  if (evt == 3){c = 'class="active"';}
+  //  Makes the actual navbar
+  var navi =  document.createElement("div");
+  navi.id = "topnav";
+  navi.innerHTML = '<a href="index.html" '+a+' >Home</a><a href="personnal.html" '+b+' >My presentations</a><a href="my_account.html" '+c+'>My account</a><i class="fas fa-bars" onclick="expandNavigation()"></i>';
+  document.getElementById("top").appendChild(navi);
+}
