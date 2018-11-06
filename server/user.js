d9fbc2696588c5f6bd83bdfaea08728c8da368a2
Suzanna Wentzel <suzannawentzel1708@gmail.com>
2018-11-06 12:29:10 +0100

    Debuggin create user

--- server/user.js
+++ server/user.js
@@ -9,22 +9,23 @@ router.post('/api/user', async function(req, res, next) {
   let userPass = req.body.password;
   let userRole = req.body.role;
 
-  /*
+
   let queryUser = `SELECT * FROM public.users t
   WHERE username = '${userName}'`;
 
-  let userExists = await db.select(queryUser);//? true : false;
+  let userExists = await db.select(queryUser) ? true : false;
 
-  console.log(userExists);
+  //console.log(userExists);
   if(userExists) {
     res.status(403).json({}).end();
-  }*/
+  } else {
+    
+    let query = `INSERT INTO "public"."users" ("id", "username", "email", "password", "role")
+    VALUES (DEFAULT, '${userName}', '${userEmail}', '${userPass}', '${userRole}')`;
 
-  let query = `INSERT INTO "public"."users" ("id", "username", "email", "password", "role")
-  VALUES (DEFAULT, '${userName}', '${userEmail}', '${userPass}', '${userRole}')`;
-
-  let status = await db.insert(query) ? 200 : 500;
-  res.status(status).json({}).end();
+    let status = await db.insert(query) ? 200 : 500;
+    res.status(status).json({}).end();
+  }
 });
 
 // Logging in
