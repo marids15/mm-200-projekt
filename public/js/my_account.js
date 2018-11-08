e488210f26388a39a1bcdf97bbfbdd9a87ab249c
R-MAN\Ragnar <ragnab07@uia.no>
2018-11-08 11:18:28 +0100

    added myaccount.html
    just to save it to gituhb_restarting computer

---
+++ public/js/my_account.js
@@ -0,0 +1,7 @@
+document.getElementById("SaveCredChanges").onclick= UpdateCreds;
+
+function UpdateCreds (){
+  let username = document.getElementById("MyUsername").value;
+  console.log(username);
+  console.log("saveClicked");
+}
