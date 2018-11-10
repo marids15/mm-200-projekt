2854927c0185147fa8d22ddba0e2bc076af66762
Suzanna Wentzel <suzannawentzel1708@gmail.com>
2018-11-11 00:59:34 +0100

    Added tokens for authorization and hashing of passwords

--- server/user.js
+++ server/user.js
@@ -1,12 +1,13 @@
 let express = require('express');
 let router = express.Router();
-var db = require("./db.js");
+const db = require("./db.js");
+const sha256 = require('js-sha256').sha256;
 
 // creating new user
 router.post('/api/user', async function(req, res, next) {
   let userName = req.body.username;
   let userEmail = req.body.email;
-  let userPass = req.body.password;
+  let userPass = sha256(process.env.SALT + req.body.password);
   let userRole = req.body.role;
 
 
@@ -24,25 +25,41 @@ router.post('/api/user', async function(req, res, next) {
       VALUES (DEFAULT, '${userName}', '${userEmail}', '${userPass}', '${userRole}') RETURNING *`;
 
     let user = await db.insert(query);
-    let status = user ? 200 : 500;
-    res.status(status).json(user).end();
+
+    let userId = user[0].id;
+    let newToken = createToken(userId);
+    let queryToken = `INSERT INTO "public".tokens ("token", "user_id") VALUES ('${newToken}', '${userId}') RETURNING *`;
+    let token = await db.insert(queryToken);
+
+    let status = (user && token) ? 200 : 500;
+    res.status(status).set('Authorization', token[0].token).json(user).end();
   }
 });
 
 // Logging in
 router.post('/api/users/auth', async function(req, res, next) {
   let userName = req.body.username;
-  let userPass = req.body.password;
+  let userPass = sha256(process.env.SALT + req.body.password);
 
   let query = `SELECT * FROM public.users t
   WHERE username = '${userName}' and password = '${userPass}'`;
 
   let user = await db.select(query);
   if(user) {
-    res.status(200).json(user).end();
+    let tokenQuery = `SELECT * FROM public.tokens t WHERE user_id = '${user[0].id}'`;
+
+    let token = await db.select(tokenQuery);
+    console.log(token[0]);
+    res.status(200).set({'Authorization': token[0].token}).json(user);
   } else {
     res.status(401).json({}).end();
   }
 });
+
+function createToken(userID) {
+  console.log('creating token...');
+  let date = new Date();
+  return sha256(process.env.TOKEN_WORD + date + userID);
+}
 
 module.exports = router;
